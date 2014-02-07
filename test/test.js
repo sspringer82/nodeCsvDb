678ae3ef92b609dafb0602f0ffbff137696a1488
Sebastian Springer <sebastian.springer@mayflower.de>
2014-02-07 10:26:15 +0100

    update and delete

--- test/test.js
+++ test/test.js
@@ -2,39 +2,59 @@ var CsvDb = require('../lib/csvDb');
 var expect = require('expect.js');
 var fs = require('fs');
 
+var file = {
+    source: 'test/data/source.txt',
+    create: 'test/data/create.txt',
+    read: 'test/data/source.txt',
+    write: 'test/data/write.txt',
+    update: 'test/data/update.txt',
+    delete: 'test/data/delete.txt'
+};
+
+var columns = ['id', 'name', 'password'];
+
 
 describe('CsvDb', function () {
     var csvDb;
 
     beforeEach(function () {
-        csvDb = new CsvDb('test/data/test.txt', ['id', 'name', 'password']);
+        csvDb = new CsvDb(file.source, columns);
     });
 
 
     it ("should instanciate with a filename as argument", function () {
-        expect(csvDb.file).to.be('test/data/test.txt');
+        expect(csvDb.file).to.be(file.source);
     });
 
     it ("should instanciate with a filename and field names as arguments", function () {
-        var csvDb = new CsvDb('dummy', ['column']);
-        expect(csvDb.file).to.be('dummy');
+        var csvDb = new CsvDb(file.source, ['column']);
+        expect(csvDb.file).to.be(file.source);
         expect(csvDb.fields).to.eql(['column']);
     });
 
     describe ("read", function () {
+        beforeEach(function () {
+            csvDb = new CsvDb(file.read, columns);
+        });
 
         it ("should read the contents of the file", function (done) {
             var promise = csvDb.getFileContent();
 
             promise.then(function (data) {
-                expect(data).to.equal('1;lala;lulu;\n2;mumu;meme;');
-                done();
+                try {
+                    expect(data).to.equal('1;lala;lulu;\n2;mumu;meme;');
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
             });
         });
 
         it ("should make an object out of a text block", function () {
             var input = '1;lala;lulu;\n2;mumu;meme;';
-            var output = [{
+            var expected = [{
                 id: 1,
                 name: 'lala',
                 password: 'lulu'
@@ -44,18 +64,18 @@ describe('CsvDb', function () {
                 password: 'meme'
             }];
 
-            expect(csvDb.transform(input)).to.eql(output);
+            expect(csvDb.transform(input)).to.eql(expected);
         });
 
         it ("should transform an empty input", function () {
             var input = '';
-            var output = [];
+            var expected = [];
 
-            expect(csvDb.transform(input)).to.eql(output);
+            expect(csvDb.transform(input)).to.eql(expected);
         });
 
         it ("should get all contents of the specified file and return an object structure", function (done) {
-            var output = [{
+            var expected = [{
                 id: 1,
                 name: 'lala',
                 password: 'lulu'
@@ -68,15 +88,19 @@ describe('CsvDb', function () {
             var promise = csvDb.get();
 
             promise.then(function (data) {
-                expect(data).to.eql(output);
-                done();
+                try {
+                    expect(data).to.eql(expected);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
             }, function (err) {
-                done();
+                done(err);
             });
         });
 
         it ("should get a specified set of data", function (done) {
-            var output = {
+            var expected = {
                 id: 2,
                 name: 'mumu',
                 password: 'meme'
@@ -85,10 +109,14 @@ describe('CsvDb', function () {
             var promise = csvDb.get(2);
 
             promise.then(function (data) {
-                expect(data).to.eql(output);
-                done();
+                try {
+                    expect(data).to.eql(expected);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
             }, function (err) {
-                done();
+                done(err);
             });
         });
 
@@ -117,39 +145,50 @@ describe('CsvDb', function () {
 
     describe("create", function () {
 
+        // @todo delete file
         beforeEach(function () {
-            if (fs.exists('test/data/create.txt')) {
-                fs.unlinkSync('test/data/create.txt');
+            if (fs.exists(file.create)) {
+                fs.unlinkSync(file.create);
             }
-            csvDb = new CsvDb('test/data/create.txt', ['id', 'name', 'password']);
+            csvDb = new CsvDb(file.create, columns);
         });
 
         it ("should get the next id of an empty file", function (done) {
-            fs.writeFileSync('test/data/create.txt', '');
+            fs.writeFileSync(file.create, '');
 
             var promise = csvDb.getNextId();
 
             promise.then(function (id) {
-                expect(id).to.eql(1);
-                done();
-            }, function () {
-                done();
+                try {
+                    expect(id).to.eql(1);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
             });
         });
 
         it ("should get the next id of a file with content", function (done) {
-            var inputFile = 'test/data/test.txt';
-            var outputFile = 'test/data/create.txt';
+
+            // @todo copy file
+            var inputFile = file.source;
+            var outputFile = file.create;
 
             fs.createReadStream(inputFile).pipe(fs.createWriteStream(outputFile));
 
             var promise = csvDb.getNextId();
 
             promise.then(function (id) {
-                expect(id).to.eql(3);
-                done();
-            }, function () {
-                done();
+                try {
+                    expect(id).to.eql(3);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
             });
         });
 
@@ -171,7 +210,7 @@ describe('CsvDb', function () {
         });
 
         it ("should insert a new data set in an empty file", function (done) {
-            fs.writeFileSync('test/data/create.txt', '');
+            fs.writeFileSync(file.create, '');
             var data = {
                 name: 'foo',
                 password: 'bar'
@@ -181,18 +220,24 @@ describe('CsvDb', function () {
             var promise = csvDb.insert(data);
 
             promise.then(function () {
-                var fileContent = fs.readFileSync('test/data/create.txt', 'utf-8');
+                try {
+                    var fileContent = fs.readFileSync(file.create, 'utf-8');
 
-                expect(fileContent).to.eql(expectedFileContent);
-                done();
-            }, function () {
-                done();
+                    expect(fileContent).to.eql(expectedFileContent);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
             });
         });
 
         it ("should append a new data set to an existing file", function (done) {
-            var inputFile = 'test/data/test.txt';
-            var outputFile = 'test/data/create.txt';
+
+            //@todo copy file
+            var inputFile = file.source;
+            var outputFile = file.create;
 
             fs.createReadStream(inputFile).pipe(fs.createWriteStream(outputFile));
 
@@ -205,24 +250,64 @@ describe('CsvDb', function () {
             var promise = csvDb.insert(data);
 
             promise.then(function () {
-                var fileContent = fs.readFileSync('test/data/create.txt', 'utf-8');
+                try {
+                    var fileContent = fs.readFileSync(file.create, 'utf-8');
 
-                expect(fileContent).to.eql(expectedFileContent);
-                done();
-            }, function () {
-                done();
+                    expect(fileContent).to.eql(expectedFileContent);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
             });
         });
     });
 
+    describe("write", function () {
+        beforeEach(function () {
+            // @todo delete file
+            if (fs.exists(file.write)) {
+                fs.unlinkSync(file.write);
+            }
+
+            csvDb = new CsvDb(file.write, columns);
+        });
+
+        it ("should write the content of a new file", function (done) {
+            var data = '1;lala;lulu;\n2;foo;meme;';
+
+            var promise = csvDb.write(data);
+
+            promise.then(function () {
+                try {
+                    var content = fs.readFileSync(file.write, 'utf-8');
+                    expect(content).to.eql(data);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
+            });
+        });
+
+    });
+
     describe("update", function () {
-        it ("should update a certain data set in an existing file", function () {
-            var inputFile = 'test/data/test.txt';
-            var outputFile = 'test/data/create.txt';
+
+        beforeEach(function () {
+            // @todo copy file
+            var inputFile = file.source;
+            var outputFile = file.update;
 
             fs.createReadStream(inputFile).pipe(fs.createWriteStream(outputFile));
 
-            var expectedFileContent = '1;lala;lulu;\n2;foo;meme;';
+            csvDb = new CsvDb(file.update, columns);
+        });
+
+        it ("should update a certain data set in an existing file", function () {
+            var expectedFileContent = '1;lala;lulu;\n2;foo;meme;2';
 
             var data = {
                 name: 'foo',
@@ -230,20 +315,49 @@ describe('CsvDb', function () {
             };
             var id = 2;
 
+
             var promise = csvDb.update(data, id);
-
             promise.then(function () {
-                var fileContent = fs.readFileSync('test/data/create.txt', 'utf-8');
+                try {
+                    var fileContent = fs.readFileSync(file.update, 'utf-8');
 
-                expect(fileContent).to.eql(expectedFileContent);
-                done();
-            }, function () {
-                done();
+                    expect(fileContent).to.eql(expectedFileContent);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, function (err) {
+                done(err);
             });
         });
     });
 
     describe("delete", function () {
 
+        beforeEach(function () {
+            // @todo copy file
+            var inputFile = file.source;
+            var outputFile = file.delete;
+
+            fs.createReadStream(inputFile).pipe(fs.createWriteStream(outputFile));
+
+            csvDb = new CsvDb(file.delete, columns);
+        });
+
+        it ("should delete a certain dataset", function (done) {
+            var expected = '1;lala;lulu;';
+
+            csvDb.delete(2).then(function () {
+                try {
+                    var fileContent = fs.readFileSync(file.delete, 'utf-8');
+
+                    expect(fileContent).to.eql(expected);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+
+        });
     });
 });
